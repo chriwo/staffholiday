ca4357135db740459006402936c81ed7404056eb
Christian Wolfram <c.wolfram@chriwo.de>
2018-09-24 08:16:51 +0200

    [CLEANUP] Code style

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -7,7 +7,8 @@ defined('TYPO3_MODE') || die();
     'LLL:EXT:staffholiday/Resources/Private/Language/locallang_flexform.xlf:plugin.title'
 );
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['staffholiday_pi1'] = 'recursive,select_key,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['staffholiday_pi1'] =
+    'recursive,select_key,pages';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['staffholiday_pi1'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
     'staffholiday_pi1',
